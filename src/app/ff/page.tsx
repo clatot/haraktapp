5ff53db3470da6e5d9bb8d20a010abee6a6d91cb
clatot <kat.clatot@gmail.com>
2024-07-29 23:46:39 +0200

    Add FFXIV Main Page

--- src/app/ff/page.tsx
+++ src/app/ff/page.tsx
@@ -1,7 +1,7 @@
 export default function FFPage() {
   return (
-    <div className="mt-16 m-auto max-w-[1200px]">
-      <h1>FFPage</h1>
+    <div className="flex flex-col items-center font-press">
+      <h1 className="text-[#FF9494] text-2xl font-black">FFXIV</h1>
     </div>
   );
 }
