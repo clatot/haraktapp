3acae290af5336a33b515f52213f7f39bffc0be1
clatot <kat.clatot@gmail.com>
2024-07-29 23:46:22 +0200

    Add Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="mt-16 m-auto max-w-[1200px]">
-      <h1>Kat Harakt's App</h1>
-    </main>
+    <div className="flex flex-col items-center gap-4 font-press text-[#FFF5E4]">
+      <h1 className="text-[#FF9494] text-2xl font-black">Kat Harakt's App</h1>
+      <Link
+        href="/"
+        className="bg-[#FF9494] text-center min-w-96 p-2 rounded-lg"
+      >
+        Home
+      </Link>
+      <Link
+        href="/ff"
+        className="bg-[#FF9494] text-center min-w-96 p-2 rounded-lg"
+      >
+        FFXIV
+      </Link>
+    </div>
   );
 }
