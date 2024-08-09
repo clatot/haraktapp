041a68b723b16a67acda0b56f243446eb2a69ff3
clatot <kat.clatot@gmail.com>
2024-08-09 02:27:35 +0200

    Add Test Data

---
+++ src/utils/weatherData.ts
@@ -0,0 +1,18 @@
+export const weatherData = {
+  newgridania: [
+    { name: "rainy", rate: 20 },
+    { name: "foggy", rate: 10 },
+    { name: "overcast", rate: 10 },
+    { name: "fair", rate: 15 },
+    { name: "clear", rate: 30 },
+    { name: "fair", rate: 15 },
+  ],
+  limsa: [
+    { name: "rainy", rate: 20 },
+    { name: "foggy", rate: 10 },
+    { name: "overcast", rate: 10 },
+    { name: "fair", rate: 15 },
+    { name: "clear", rate: 30 },
+    { name: "fair", rate: 15 },
+  ],
+};
