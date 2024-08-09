b858abcd35e7be1548aa914f2e7600297b3cd15e
clatot <kat.clatot@gmail.com>
2024-08-09 02:27:47 +0200

    Add Link on page ffxiv

--- src/app/ff/page.tsx
+++ src/app/ff/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function FFPage() {
   return (
     <div className="flex flex-col items-center font-press">
       <h1 className="text-[#FF9494] text-2xl font-black">FFXIV</h1>
+      <Link href="/ff/weather">Weather</Link>
     </div>
   );
 }
